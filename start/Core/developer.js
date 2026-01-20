24d73a64b9124b992bcbf318f684155dcb29cb7e
vinicbot-dev <devkelvin903@gmail.com>
2026-01-20 23:21:24 +0300

    Add files via upload

--- start/Core/developer.js
+++ start/Core/developer.js
@@ -1,21 +1,19 @@
 /* m
   -! Credits By Kevintech 
-  Thanks to great lonelysaam 
-  Thanks to Malvin King 
   https://wa.me/256742932677
 */
 
 // setting/config.js
 const fs = require('fs');
 
-global.owner = ["256742932677"];  
-global.status = true; // true = public, false = private
-global.versions = "1.4.0";
+global.owner = ["Kelvin Tech"];  
+global.status = false; // true = public, false = private
+global.versions = "v1.0.0";
 
 // ========= Setting WM ========= //
 global.packname = 'Vesper';
 global.author = 'Bot';
-global.wm = '© Vesper-Xmd is awesome 🔥';
+global.wm = '©Vesper-Xmd is awesome 🔥';
 
 // === For only developer ============
 global.api = "https://xploaderapi-f5e63b.platform.cypherx.space";
@@ -23,6 +21,7 @@ global.wwe = "https://www.wwe.com/api/news";
 global.wwe1 = "https://www.thesportsdb.com/api/v1/json/3/searchfilename.php?e=wwe";
 global.wwe2 = "https://www.thesportsdb.com/api/v1/json/3/searchevents.php?e=wrestling";
 global.falcon = "https://flowfalcon.dpdns.org";
+global.siputzx = "https://api.siputzx.my.id"; 
 
 global.gcount = {
   prem: 500,
@@ -32,8 +31,9 @@ global.gcount = {
 global.limitCount = 10;
 
 global.mess = {
-  group: "This not group!*",
+  group: "This not group!",
   notadmin: "This command is only preserved for group admins!",
+  notgroup: "This command can only be used in groups!",
   owner: "This command is only preserved for bot owner and sudo!",
   done: "Mission complete ✅",
   notext: "Please provide the necessary text",
