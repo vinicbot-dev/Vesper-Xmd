3c00d0ba4c958fff0338d1b6a54391f4d3b52613
vinicbot-dev <devkelvin903@gmail.com>
2026-02-21 00:59:19 +0300

    Update developer.js

--- start/Core/developer.js
+++ start/Core/developer.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 
 global.owner = ["Kelvin Tech"];  
 global.status = false; // true = public, false = private
-global.versions = "v1.2.0";
+global.versions = "v1.2.1";
 global.botname = "Vesper-Xmd"; 
 
 // ========= Other Global Settings ========= //
