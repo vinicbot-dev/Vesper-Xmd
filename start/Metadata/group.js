9d2c715fb2b5cf20e927e589a79a64c80958983b
vinicbot-dev <devkelvin903@gmail.com>
2026-02-26 16:19:22 +0300

    Add files via upload

--- start/Metadata/group.js
+++ start/Metadata/group.js
@@ -4,10 +4,10 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Create a separate database connection for group messages
-const dbPath = path.join(__dirname, './data/group.db');
+const dbPath = path.join(__dirname, './src/group.db');
 const groupDb = new sqlite3.Database(dbPath, (err) => {
   if (err) console.error('Group database connection error:', err);
-  else console.log('[GROUP-DB] Connected to group database');
+  else console.log('[Vesper-Xmd] Connected to group database');
 });
 
 // Create table if it doesn't exist
